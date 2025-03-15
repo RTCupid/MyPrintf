d64a14ab241043c785f64a135694b6baa712f15d
RTCupid <19apr2006@vk.com>
2025-03-15 10:36:03 +0300

    + one flags for NASM from documentation

--- MyPrintf.s
+++ MyPrintf.s
@@ -2,10 +2,12 @@
 ;                                  MyPrintf
 ;                         (c) 2025 Muratov Artyom
 ;------------------------------------------------------------------------------
-; compilation with:
-; nasm -f elf64 -l 1-nasm.lst 1-nasm.s  ;  ld -s -o 1-nasm 1-nasm.o
+; Compilation with:                     Flags:
+; nasm -f elf64 -l 1-nasm.lst 1-nasm.s  -w+orphan-labels
+; Linker:
+; ld -s -o 1-nasm 1-nasm.o
 
-section .text
+section     .text
 
 global _start                  ; predefined entry point name for linker
 
@@ -18,6 +20,19 @@ _start:     mov rax, 0x01      ; write64 (rdi, rsi, rdx) ... r10, r8, r9
             mov rax, 0x3C      ; exit64 (rdi)
             xor rdi, rdi
             syscall
+
+;------------------------------------------------------------------------------
+; MyPrintf  my function printf version 11.1, write to console string
+;           with some arguments that pinned by '%'
+; Entry:    parametres in stack (type "cdecl")
+; Exit:
+; Destroy:
+;------------------------------------------------------------------------------
+;MyPrintf    proc
+
+
+;            ret
+;MyPrintf    endp
 
 section     .data
 
