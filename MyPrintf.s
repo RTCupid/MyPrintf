4f2b3e4f5a8e6e60b4931aedd3e60895d77069dc
RTCupid <19apr2006@vk.com>
2025-03-17 16:19:11 +0300

    +Meow

--- MyPrintf.s
+++ MyPrintf.s
@@ -11,16 +11,27 @@ section     .text
 
 global _start                  ; predefined entry point name for linker
 
-_start:     mov rax, 0x01      ; write64 (rdi, rsi, rdx) ... r10, r8, r9
-            mov rdi, 1         ; stdout
-            mov rsi, Msg
-            mov rdx, MsgLen    ; strlen (Msg)
-            syscall
+_start:     call _Meow
 
             mov rax, 0x3C      ; exit64 (rdi)
             xor rdi, rdi
             syscall
 
+;------------------------------------------------------------------------------
+; Meow      my function write to console Meow
+; Entry:    None
+; Exit:     None
+; Destroy:  rax, rdi, rsi, rdx
+;------------------------------------------------------------------------------
+
+_Meow:
+            mov rax, 0x01      ; write64 (rdi, rsi, rdx) ... r10, r8, r9
+            mov rdi, 1         ; stdout
+            mov rsi, Msg
+            mov rdx, MsgLen    ; strlen (Msg)
+            syscall
+
+            ret
 ;------------------------------------------------------------------------------
 ; MyPrintf  my function printf version 11.1, write to console string
 ;           with some arguments that pinned by '%'
