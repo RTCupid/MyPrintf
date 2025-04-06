307804eedb8804aeba1925d919f4668905ca8afd
RTCupid <19apr2006@vk.com>
2025-04-06 10:58:53 +0300

    Update main.cpp, tests completed, delete comments

--- main.cpp
+++ main.cpp
@@ -4,21 +4,10 @@ extern "C" void _MyFastCallPrintf (const char* format, ...);
 
 int main ()
 {
-
-    for (int i = 100; i > 0; i--)
-    {
-        _MyFastCallPrintf ("Mikhalina one %s %d %%\n", "love", 100);
-    }
-
     _MyFastCallPrintf ("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b", -1, -1, "love", 3802, 100, 33, 127,
                                                                  -1, "love", 3802, 100, 33, 127);
 
-    //_MyFastCallPrintf ("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b", -1, -1, "love", 3802, 100, 33, 127,
-    //                                                             -1, "love", 3802, 100, 33, 127);
-
-
-
+    _MyFastCallPrintf ("%o\n%d %s %x %d%%%c%b\n%d %s %x %d%%%c%b", -1, -1, "love", 3802, 100, 33, 127,
+                                                                 -1, "love", 3802, 100, 33, 127);
     return 0;
 }
-
-// ld -s -o build/bin/MyPrintf build/obj/main.o build/obj/MyPrintf.o -lc -dynamic-linker /lib64/ld-linux-x86-64.so.2 -fsanitize=address
